1eca4deb603993dbbdea076ea5c8a567a3c39c5c
Raymond Dormien <raymond.dormien@bcmsolutions.de>
2016-02-17 16:56:17 +0000

    fix for api doc task - usage changed with new version

--- tasks/documentation/api.js
+++ tasks/documentation/api.js
@@ -18,12 +18,12 @@ module.exports = gulp.task('doc-api', function (cb) {
     catch (ex) {}
 
     if (runApiDoc) {
-        apidoc.exec({
+        apidoc({
             src: global.config.files.doc.src.api,
             dest: global.config.files.doc.dest.api,
             debug: global.DEBUG,
             includeFilters: [".*\\.js$"]
-        });
+        }, cb);
     }
     else {
         cb();
